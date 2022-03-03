1d7cc6f7fcf81236191476f7ac2ac8c12a5472e6
seppeV2 <seppe.vilain@student.kuleuven.be>
2022-03-03 22:27:15 +0100

    extra changes

--- Matlab/bufferCoef.m
+++ Matlab/bufferCoef.m
@@ -1,12 +1,25 @@
-Pt=119;
-Pa=3491;
-Pr=1745;
+clear all
+Pt=1745;
+Pr=0;
+Pa=22515;
 p=60;
-lambda=2;
-[idealBufferRound, idealBuffer, nCoef, pCoef, C] = calculateIdealBuffer(lambda,p,Pt,Pr,Pa,1);
-XPoints1 = [0, idealBuffer, 2*idealBuffer];
-YPoints1 = [double(subs(C,0)), double(subs(C,idealBuffer)),double(subs(C,2*idealBuffer))];
+lambda=5;
+[idealBufferRound, idealBuffer , nCoef, pCoef, C] = calculateIdealBuffer(lambda,p,Pt,Pr,Pa,1);
 
+i=5;
+j=5;
+if idealBuffer < j
+    j=idealBuffer;
+end
+nCoef=double((subs(C,(idealBuffer - j)) - subs(C,idealBuffer))/(j*10000));
+pCoef=double((subs(C,idealBuffer+i) - subs(C,idealBuffer))/(i*10000));
 
+XPoints1 = [idealBuffer-j, idealBuffer, idealBuffer+i];
+YPoints1 = [double(subs(C,idealBuffer-j)), double(subs(C,idealBuffer)),double(subs(C,idealBuffer+i))];
+
+disp('nCoef');
+disp(nCoef);
+disp('pCoef');
+disp(pCoef);
 hold on 
 plot(XPoints1, YPoints1);
--- Matlab/calculateIdealBuffer.m
+++ Matlab/calculateIdealBuffer.m
@@ -1,4 +1,6 @@
-function [idealBufferRound, idealBuffer, nCoef, pCoef, C] = calculateIdealBuffer(lambda,p,Pt,Pr,Pa,plot)
+function [idealBufferRound, idealBuffer, pCoef, nCoef, C] = calculateIdealBuffer(lambda,p,Pt,Pr,Pa,plot)
+
+clear all
 
 syms B real
 
@@ -6,7 +8,7 @@ C = 2.2*Pt*p*exp(-(1/lambda) * B) + (2*Pt+1.5*Pr)*(B+lambda*(exp(-(1/lambda)*B)-
     +2.5*Pa*lambda*exp(-(1/lambda)*B); %generalised cost function
 if plot
 hold on
-fplot(C, [0,16]); %plot of the cost function
+fplot(C, [0,30]); %plot of the cost function
 end
 
 C2 = diff(C , B); %first derivative
@@ -30,8 +32,15 @@ disp(vpa(subs(C,idealBuffer),6));
 disp('y value 0');
 disp(vpa(subs(C,0),6));
 
-nCoef=double((subs(C,0) - subs(C,idealBuffer))/(idealBuffer*10000));
-pCoef=double((subs(C,2*idealBuffer) - subs(C,idealBuffer))/((2*idealBuffer-idealBuffer)*10000));
+i=5;
+j=5;
+if idealBuffer < j
+    j=idealBuffer;
+end
+nCoef=double((subs(C,(idealBuffer - j)) - subs(C,idealBuffer))/(j*10000));
+pCoef=double((subs(C,idealBuffer+i) - subs(C,idealBuffer))/(i*10000));
+
+
 
 end
 
